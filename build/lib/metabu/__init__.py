477878f06862357099d3023df676241048c72868
luxusg1 <milijaonalouisot@gmail.com>
2022-02-26 15:48:19 +0300

    remove useless file

--- build/lib/metabu/__init__.py
+++
@@ -1,5 +0,0 @@
-from metabu.utils import *
-from metabu.fused_gromov_wasserstein import *
-from metabu.prior_distance import *
-from metabu.learn_metafeatures import *
-from metabu.relational_autoencoders import *
