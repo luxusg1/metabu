4882745639abbbdf1c8601648f3c36ca81ed6755
louisot <milijaonalouisot@gmail.com>
2022-03-18 00:52:45 +0300

    remove relational_encoders directory && change all notations to match paper

--- metabu/fused_gromov_wasserstein.py
+++ metabu/fused_gromov_wasserstein.py
@@ -4,7 +4,7 @@ from sklearn.cluster import AgglomerativeClustering
 from sklearn.manifold import MDS
 from sklearn.preprocessing import MinMaxScaler
 
-from metabu.relational_autoencoders.Methods import prae
+from metabu.prae import fgw
 from metabu.utils import intrinsic_estimator
 
 
@@ -17,27 +17,31 @@ def remove_outlier(labels, datasets_has_priors, cost_matrix):
            ~np.in1d(np.arange(len(cost_matrix)), outlier)]
 
 
-def train_fused_gromov_wasserstein(data_train, cost_matrix, lr=0.001, seed=42,
-                                   early_stopping=20, log_tensorboard=False, dim_out=None, **kwargs):
+def train_fused_gromov_wasserstein(basic_representation_train,
+                                   basic_representation_test,
+                                   cost_matrix,
+                                   lr=0.001, seed=42,
+                                   early_stopping=20,
+                                   **kwargs):
     print("Train embedding with Fused-Gromov-Wasserstein loss")
-    x_train, idx_train = data_train
+
+    x_train, idx_train = basic_representation_train
 
     l1 = kwargs.get("l1", 0.001)
     alpha = kwargs.get("alpha", 0.5)
     print(kwargs, l1, alpha)
 
+    task_has_target_representation_used_for_train = kwargs["task_has_target_representation_used_for_train"]
 
-
-    datasets_has_priors_use_for_train = kwargs["datasets_has_priors_use_for_train"]
-    print(len(datasets_has_priors_use_for_train))
-    N_old = len(datasets_has_priors_use_for_train)
+    N_old = len(task_has_target_representation_used_for_train)
     cluster = AgglomerativeClustering(affinity="precomputed", linkage="complete", n_clusters=5)
     cluster.fit(cost_matrix)
-    datasets_has_priors_use_for_train, labels_used, cost_matrix_used = remove_outlier(cluster.labels_,
-                                                                                      datasets_has_priors_use_for_train,
-                                                                                      cost_matrix)
+    task_has_target_representation_used_for_train, labels_used, cost_matrix_used = \
+        remove_outlier(cluster.labels_,
+                       task_has_target_representation_used_for_train,
+                       cost_matrix)
 
-    N = len(datasets_has_priors_use_for_train)
+    N = len(task_has_target_representation_used_for_train)
     dim_in = x_train.shape[1]
     dim_out = intrinsic_estimator(cost_matrix_used)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -48,6 +52,8 @@ def train_fused_gromov_wasserstein(data_train, cost_matrix, lr=0.001, seed=42,
 
     xs = torch.from_numpy(scaler.transform(x_train.values)).float().to(device)
     xt = torch.from_numpy(X_true).float().to(device)
+
+    x_test = torch.from_numpy(scaler.transform(basic_representation_test.values)).to(device).float()
 
     # M /= M.sum(0)
 
@@ -63,10 +69,10 @@ def train_fused_gromov_wasserstein(data_train, cost_matrix, lr=0.001, seed=42,
 
     for i in range(100000):
         optimizer.zero_grad()
-        x = xs[[np.random.choice(idx_train[id]) for id in datasets_has_priors_use_for_train]]
+        x = xs[[np.random.choice(idx_train[id]) for id in task_has_target_representation_used_for_train]]
         x_new = model(x)
-        loss = prae.fgw(source=x_new, target=xt, cost_source_target=None, device=device, beta=alpha,
-                        M=M) + l1 * torch.norm(model.weight, 1)
+        loss = fgw(source=x_new, target=xt, cost_source_target=None, device=device, beta=alpha,
+                   M=M) + l1 * torch.norm(model.weight, 1)
         loss.backward()
         optimizer.step()
 
@@ -78,4 +84,4 @@ def train_fused_gromov_wasserstein(data_train, cost_matrix, lr=0.001, seed=42,
         no_improvement += 1
         print(f"Epoch test {i}: ", loss.item())
 
-    return model, model(xs[:N_old]).cpu().detach().numpy(), None
+    return model, model(xs[:N_old]).cpu().detach().numpy(), model(x_test).cpu().detach().numpy()
