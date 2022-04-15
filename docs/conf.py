8fa03b03806d117b1697294b220c85cf01be22c3
HR <rkt.herilalaina@gmail.com>
2022-04-15 13:31:29 +0200

    improve docs

--- docs/conf.py
+++ docs/conf.py
@@ -18,8 +18,8 @@ sys.path.insert(0, os.path.abspath('../'))
 # -- Project information -----------------------------------------------------
 
 project = 'Metabu'
-copyright = '2022, Herilalaina Rakotoarison and Louisot Milijaona'
-author = 'Herilalaina Rakotoarison and Louisot Milijaona'
+copyright = '2022, Rakotoarison, Milijaona, Rasoanaivon, Sebag, Schoenauer'
+author = 'Herilalaina Rakotoarison, Louisot Milijaona, Andry Rasoanaivon, Michele Sebag, and Marc Schoenauer'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
@@ -30,20 +30,38 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc', 'numpydoc'
+    "sphinx.ext.autodoc",  # Core Sphinx library for auto html doc generation from docstrings
+    # "sphinx.ext.autosummary",  # Create neat summary tables for modules/classes/methods etc
+    # "sphinx.ext.intersphinx",  # Link to other project's documentation (see mapping below)
+    # "sphinx_autodoc_typehints",  # Automatically document param types (less noise in class signature)
+    "numpydoc",
+    'sphinxcontrib.bibtex',
+    # "sphinx.ext.linkcode",
 ]
 
+numpydoc_show_class_members = False
+autosummary_generate = True
 
+# numpydoc_show_class_members = False
+
+
+# generate autosummary even if no references
+# autosummary_generate = False
+# autosummary_imported_members = False
+
+
+# Biblio
+bibtex_bibfiles = ['refs.bib']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-autodoc_typehints = "description"
+autodoc_typehints = "signature"
 
 # Don't show class signature with the class' name.
 autodoc_class_signature = "separated"
@@ -53,20 +71,28 @@ autodoc_class_signature = "separated"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+import sphinx_readable_theme
+html_theme = 'readable'
+html_theme_path = [sphinx_readable_theme.get_html_theme_path()]
+
 
 html_theme_options = {
-    "display_github": True,
-    'github_user': 'luxusg1',
-    'github_repo': 'Metabu',
-    'github_button': False,
-    'github_banner': True,
+    # "display_github": True,
+    # 'github_user': 'luxusg1',
+    # 'github_repo': 'Metabu',
+    # 'github_button': False,
+    # 'github_banner': True,
     #'nosidebar': True,
     #'page_width': '1000px'
+    # "sticky_navigation": True
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 html_title = "Learning Meta-features for AutoML"
+
+# add custom css
+# def setup(app):
+#   app.add_css_file('custom.css')  # give a filename you created.
--- docs/index.rst
+++ docs/index.rst
@@ -1,6 +1,16 @@
 .. toctree::
    :maxdepth: 2
 
+Metabu: Learning Meta-Features
+-------------------------------
+
+This work tackles the AutoML problem :cite:p:`hutter_automated_2019`, aimed to automatically select an ML algorithm and its hyper-parameter configuration most appropriate to the dataset at hand. The proposed approach, MetaBu, learns new meta-features via an Optimal Transport procedure :cite:p:`titouan_optimal_2019`, aligning the manually designed meta-features :cite:p:`rivolli_meta-features_2022` with the space of distributions on the hyper-parameter configurations. MetaBu meta-features, learned once and for all, induce a topology on the set of datasets that is exploited to define a distribution of promising hyper-parameter configurations amenable to AutoML. Experiments on the OpenML CC-18 benchmark :cite:p:`bischl_openml_2017` demonstrate that using MetaBu meta-features boosts the performance of state of the art AutoML systems, AutoSklearn :cite:p:`feurer_efficient_2015` and Probabilistic Matrix Factorization :cite:p:`fusi_probabilistic_2018`. Furthermore, the inspection of MetaBu meta-features gives some hints into when an ML algorithm does well. Finally, the topology based on MetaBu meta-features enables to estimate the intrinsic dimensionality of the OpenML benchmark w.r.t. a given ML algorithm or pipeline.
+
+.. image:: ../illustration_metabu.png
+   :align: center
+   :width: 600
+
+
 
 Installation
 -------------
@@ -12,15 +22,13 @@ Install with pip:
    python setup.py install
 
 
-Use Singularity:
+or with Singularity:
 
 * build *local* container with definition file `env/metabu.def`.
 * fetch *remote* container from (coming soon).
 
-Usage
--------------
-
-Simple to use:
+Example
+--------
 
 .. code-block:: python
 
@@ -40,17 +48,10 @@ Experiments
 
 > Script to reproduce experiments will be available under the **experiments** branch.
 
+
 API
-----------
-
-.. tip::
-
-    All next cited parameters are also attributes of Metabu class.
-
-
-The `metabu.metabu.Metabu` class
------------------------------------
-.. autoclass:: metabu.metabu.Metabu
+---
+.. autoclass:: metabu.Metabu
    :members:
 
 
@@ -61,11 +62,15 @@ Cite Metabu
 
    @inproceedings{rakotoarison2022learning,
     title       = {Learning meta-features for Auto{ML}},
-    author      = {Herilalaina Rakotoarison and Louisot Milijaona and Andry RASOANAIVO and Michele Sebag and Marc Schoenauer},
+    author      = {Herilalaina Rakotoarison and Louisot Milijaona and Andry Rasoanaivo and Michele Sebag and Marc Schoenauer},
     booktitle   = {International Conference on Learning Representations},
     year        = {2022},
     url         = {https://openreview.net/forum?id=DTkEfj0Ygb8}
    }
 
 
+References
+-----------
+
+.. bibliography::
 
