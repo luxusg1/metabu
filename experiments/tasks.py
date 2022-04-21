c0561eed32d8e38adbbf98de5d74308fae9c2e76
HR <rkt.herilalaina@gmail.com>
2022-04-21 19:52:58 +0200

    refractor

--- experiments/tasks.py
+++ experiments/tasks.py
@@ -23,6 +23,34 @@ def get_target_representations(pipeline, path):
         os.path.join(path, "top_preprocessed_target_representation", pipeline.name + "_target_representation.csv"))
 
 
+def get_metabu_representations(cfg, basic_reprs, target_reprs, list_ids, train_ids, test_ids):
+    basic_reprs["boostrap"] = 0
+
+    bootstrap_reprs = get_bootstrap_representations(metafeature=cfg.metafeature, path=cfg.data_path)
+    bootstrap_reprs = bootstrap_reprs[bootstrap_reprs.task_id.isin(list_ids)]
+    bootstrap_reprs["boostrap"] = 1
+
+    combined_basic_reprs = pd.concat([basic_reprs, bootstrap_reprs], axis=0)
+    combined_basic_reprs = pd.concat([
+        combined_basic_reprs[combined_basic_reprs.task_id.isin(train_ids)],
+        combined_basic_reprs[combined_basic_reprs.task_id.isin(test_ids)]
+    ], axis=0)
+
+    metabu = Metabu(verbose=False, seed=cfg.seed)
+    repr_train, repr_test = metabu.train_predict(
+        basic_reprs=combined_basic_reprs.drop(["boostrap"], axis=1),
+        target_reprs=target_reprs,
+        column_id="task_id",
+        train_ids=train_ids,
+        test_ids=test_ids
+    )
+    metabu_reprs = np.concatenate([repr_train, repr_test], axis=0)
+    metabu_reprs = pd.DataFrame(metabu_reprs, columns=[f"col{_}" for _ in range(metabu_reprs.shape[1])])
+    metabu_reprs["task_id"] = combined_basic_reprs["task_id"].values
+    metabu_reprs["boostrap"] = combined_basic_reprs["boostrap"].values
+    return metabu_reprs[metabu_reprs.boostrap == 0].drop(["boostrap"], axis=1)
+
+
 def run_task1(cfg):
     target_reprs = get_target_representations(pipeline=cfg.pipeline, path=cfg.data_path)
     list_ids = sorted(list(target_reprs["task_id"].unique()))
@@ -36,31 +64,8 @@ def run_task1(cfg):
     if cfg.metafeature.name == "metabu":
         train_ids = [_ for _ in list_ids if _ != cfg.openml_tid]
         test_ids = [cfg.openml_tid]
-        basic_reprs["boostrap"] = 0
 
-        bootstrap_reprs = get_bootstrap_representations(metafeature=cfg.metafeature, path=cfg.data_path)
-        bootstrap_reprs = bootstrap_reprs[bootstrap_reprs.task_id.isin(list_ids)]
-        bootstrap_reprs["boostrap"] = 1
-
-        combined_basic_reprs = pd.concat([basic_reprs, bootstrap_reprs], axis=0)
-        combined_basic_reprs = pd.concat([
-            combined_basic_reprs[combined_basic_reprs.task_id.isin(train_ids)],
-            combined_basic_reprs[combined_basic_reprs.task_id.isin(test_ids)]
-        ], axis=0)
-
-        metabu = Metabu(verbose=False, seed=cfg.seed)
-        repr_train, repr_test = metabu.train_predict(
-            basic_reprs=combined_basic_reprs.drop(["boostrap"], axis=1),
-            target_reprs=target_reprs,
-            column_id="task_id",
-            train_ids=train_ids,
-            test_ids=test_ids
-        )
-        metabu_reprs = np.concatenate([repr_train, repr_test], axis=0)
-        metabu_reprs = pd.DataFrame(metabu_reprs, columns=[f"col{_}" for _ in range(metabu_reprs.shape[1])])
-        metabu_reprs["task_id"] = combined_basic_reprs["task_id"].values
-        metabu_reprs["boostrap"] = combined_basic_reprs["boostrap"].values
-        basic_reprs = metabu_reprs[metabu_reprs.boostrap == 0]
+        basic_reprs = get_metabu_representations(cfg, basic_reprs, target_reprs, list_ids, train_ids, test_ids)
 
     basic_reprs = basic_reprs.set_index("task_id")
 
@@ -88,3 +93,5 @@ def run_task1(cfg):
                 get_ndcg_score(dist_pred=np.array([pred_dist[id_test]]), dist_true=np.array([true_dist[id_test]]),
                                k=cfg.task.ndcg)
             ))
+
+
